fdfbf3018a60fac8bdb8f433e4fecf3531d37e15
Кирилл <k_kirill45@mail.ru>
2022-04-29 12:02:18 +0300

    Create models

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,31 @@
+import mptt.fields
 from django.db import models
+from django.contrib.auth import get_user_model
+from mptt.models import MPTTModel
 
-# Create your models here.
+User = get_user_model()
+
+
+class Post(models.Model):
+    text = models.TextField()
+    pub_date = models.DateTimeField(
+        auto_now_add=True
+    )
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='posts'
+    )
+
+
+class Comment(MPTTModel):
+    text = models.TextField()
+    post = models.ForeignKey(
+        Post, on_delete=models.CASCADE, related_name='comments'
+    )
+    parent = mptt.fields.TreeForeignKey(
+        'self', on_delete=models.CASCADE, related_name='children',
+        null=True, blank=True
+    )
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='comments'
+    )
+
